ee510ad8dd28ad792ecbdb8789588228afad6c9b
FR4NC31 <francisedgard16@gmail.com>
2026-02-03 16:32:37 +0800

    feat: set up initial backend server with Express app, database connection, and API routes.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,12 +11,11 @@ const app = express()
 app.use(express.json())
 
 app.get("/health", (req, res) => {
-    res.json({ status: "ok", message: "Server is Running and still goods" })
+    res.json({ status: "ok", message: "Server is running" })
 })
 
-app.use("api/auth", authRoutes)
-app.use("api/chats", chatRoutes)
-app.use("api/messages", messageRoutes)
-app.use("api/users", userRoutes)
-
+app.use("/api/auth", authRoutes)
+app.use("/api/chats", chatRoutes)
+app.use("/api/messages", messageRoutes)
+app.use("/api/users", userRoutes)
 export default app
