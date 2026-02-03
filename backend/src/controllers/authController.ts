30d132ad1bde48bcad9cf328b279a892f84ecdf3
FR4NC31 <francisedgard16@gmail.com>
2026-02-03 19:02:12 +0800

    feat: Implement initial backend API with Clerk authentication and basic web frontend setup.

---
+++ backend/src/controllers/authController.ts
@@ -0,0 +1,50 @@
+import type { AuthRequest } from "../middlewares/auth";
+import type { NextFunction, Response } from "express";
+import { User } from "../models/user";
+import { clerkClient, getAuth, requireAuth } from "@clerk/express";
+
+export async function getMe(req: AuthRequest, res: Response, next: NextFunction) {
+    try {
+        const userId = req.userId
+
+        const user = await User.findById(userId)
+
+        if (!user) return res.status(404).json({ message: "User not found" })
+
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500){
+            next()
+        }
+    }
+}
+
+export async function authCallback(req: Request, res: Response, next: NextFunction) {
+    try {
+        const {userId: clerkId} = getAuth(req)
+
+        if(!clerkId) {
+            res.status(401).json({message: "Unauthorized"})
+            return
+        }
+        
+
+        let user = await User.findOne({clerkId})
+
+        if(!user){
+            const clerkUser = await clerkClient.users.getUser(clerkId)
+
+            user = await User.create({
+                clerkId,
+                name: clerkUser.firstName ? `${clerkUser.firstName} ${clerkUser.lastName || ""}`.trim(): clerkUser.emailAddresses[0]?.emailAddress.split("@")[0] || "User", 
+                email: clerkUser.emailAddresses[0]?.emailAddress,
+                avatar: clerkUser.imageUrl
+            })
+        }
+
+        res.json(user)
+    } catch (error) {
+        res.status(500).json({message: "Internal server error"})
+        next(error)
+    }
+}
